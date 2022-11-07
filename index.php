9ef80d82dcb3415409c6dc56f4fbd5da497b5cd0
JUANCARLITROS44 <juancarloslerma@gmail.com>
2022-11-07 19:36:14 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -1,39 +1,90 @@
-<html lang="pt-BR">
+<?php
+    require_once 'conexion.php';
+?>
+
+<!DOCTYPE html>
+<html lang="es">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Sistema de login</title>
-    <link rel="stylesheet" href="css/estilo.css">
+
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Index</title>
+
+	<!-- Bootstrap -->
+	<link href="css/bootstrap.min.css" rel="stylesheet">
+	<link href="css/style_nav.css" rel="stylesheet">
+
+	<style>
+		.content {
+			margin-top: 80px;
+		}
+	</style>
 </head>
 
 <body>
-    <div id="corpo-center">
-        <h1>Login</h1><!-- titulo -->R
-        <?php  
-            require_once "login.php";            
-            if(isset($_POST['btn-entrar']) ){// boton registrar
+	<nav class="navbar navbar-default navbar-fixed-top">
+		<?php include('nav.php');?>
+	</nav>
 
-            // pegando input
-            $email      = addslashes($_POST['email']);
-            $password   = addslashes($_POST['password']);           
+	<div class="container">
+		<div class="content">
+			<h2>Indice &raquo; </h2>
+			<hr />
 
-            // inserindo
-            $logar = new Login();
-            $logar-> logar($email, $password);
-            }  
-        ?>
-        <!-- Formulario -->
-        <form action="" method="post">
-            <input type="email" name="email" placeholder="Email" required="">
-            <input type="password" name="password" placeholder="Password" required=""> 
+            <?php  
+                require_once "login.php";            
+                if(isset($_POST['btn-entrar']) ){// boton registrar
 
-            <!-- Botones -->
-            <button type="submit" name="btn-entrar"> Entrar </button>
-            <a href="registro.php"> Estas registrado? <strong>  Registrate ahora!!</strong> </a>
-        </form>
+                // pegando input
+                $email      = addslashes($_POST['email']);
+                $password   = addslashes($_POST['password']);           
+
+                // inserindo
+                $logar = new Login();
+                $logar-> logar($email, $password);
+                }  
+            ?>
+
+            <!-- Formulario -->
+            <div class="form-row align-items-center mb-3 p-3">            
+				<form action="" method="post">
+
+					<input type="email" name="email" placeholder="Email" required=""> <br><br>   
+					<input type="password" name="password" placeholder="Password" required=""> <br><br>         							
+
+					<!-- Botones -->
+					<button type="submit" name="btn-entrar"> Entrar </button> <br><br>   
+					<a href="registro.php"> Estas registrado? <strong>  Registrate ahora!!</strong> </a>     
+				</form>
+			</div> 
+			
+        </div>
     </div>
+	<hr>
 
-    <footer> JLM </footer>
+	</div><center>
+	<p>&copy; Proyecto DAW <?php echo date("Y");?></p
+		</center>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
 </body>
-
 </html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- registro.php
+++ registro.php
@@ -1,53 +1,76 @@
-<html lang="pt-BR">
+<?php
+    require_once 'conexion.php';
+?>
 
+<!DOCTYPE html>
+<html lang="es">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Sistema de login</title>
-    <link rel="stylesheet" href="css/estilo.css">
 
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Index</title>
+
+	<!-- Bootstrap -->
+	<link href="css/bootstrap.min.css" rel="stylesheet">
+	<link href="css/style_nav.css" rel="stylesheet">
+
+	<style>
+		.content {
+			margin-top: 80px;
+		}
+	</style>
 </head>
 
 <body>
-    <div id="corpo-center" style="margin-top: 8vh;">
+    <nav class="navbar navbar-default navbar-fixed-top">
+		<?php include('nav.php');?>
+	</nav>
 
-        <h1> Registro</h1><!-- titulo -->
+	<div class="container">
+		<div class="content">
+			<h2>Indice &raquo; </h2>
+			<hr />
+            <?php
+                require_once "login.php";
 
-        <?php
-        
-            require_once "login.php";
+                if( isset($_POST['btn-registrar']) ){// boton registrar
 
-            if( isset($_POST['btn-registrar']) ){// boton registrar
+                    // pegando input
+                    $nombre     = addslashes($_POST['nombre']);
+                    $apellidos  = addslashes($_POST['apellidos']);
+                    $email      = addslashes($_POST['email']);
+                    $password   = addslashes($_POST['password']);
+                    $passwordConf = addslashes($_POST['passwordConf']);
 
-                // pegando input
-                $nombre     = addslashes($_POST['nombre']);
-                $apellidos  = addslashes($_POST['apellidos']);
-                $email      = addslashes($_POST['email']);
-                $password   = addslashes($_POST['password']);
-                $passwordConf = addslashes($_POST['passwordConf']);
+                    // inserindo
+                    $registrar = new Login();
+                    $registrar-> registro($nombre, $apellidos, $email ,$password, $passwordConf);
+                }         
+            ?>
 
-                // inserindo
-                $registrar = new Login();
-                $registrar-> registro($nombre, $apellidos, $email ,$password, $passwordConf);
+            <!-- Formulario -->
+            <div class="form-row align-items-center mb-3 p-3">    
+                <form action="" method="post">  
+                    <input type="text" name="nombre" placeholder="nombre" > <br><br> 
+                    <input type="text" name="apellidos" placeholder="apellidos" > <br><br>
+                    <input type="email" name="email" placeholder="Email"> <br><br>
+                    <input type="password" name="password" placeholder="password" > <br><br>
+                    <input type="password" name="passwordConf" placeholder="Confirmar password"> <br><br>
 
-            }
-         
-        ?>
-
-        <!-- Formulario -->
-        <form action="" method="post">  
-            <input type="text" name="nombre" placeholder="nombre"  >
-            <input type="text" name="apellidos" placeholder="apellidos" >
-            <input type="email" name="email" placeholder="Email" >
-            <input type="password" name="password" placeholder="password"  >
-            <input type="password" name="passwordConf" placeholder="Confirmar password" >
-            <!-- Botones -->
-            <button type="submit" name="btn-registrar"> registrar </button>
-            <a href="index.php"> Ja sou inscrito. <strong> Fazer login!!</strong></a>
-        </form>
-
-        <footer> JLM </footer>
+                    <!-- Botones -->
+                    <button type="submit" name="btn-registrar"> Registrar </button> <br><br>
+                    <a href="index.php"> Ya estas inscrito. <strong> Vovler a login!!</strong></a>
+                </form>
+            </div> 
+        </div> 
     </div>
-</body>
+	<hr>
 
+	</div><center>
+	<p>&copy; Proyecto DAW <?php echo date("Y");?></p
+		</center>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+</body>
 </html>
